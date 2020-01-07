b3e03717896c9d1a98d5edc0e13a324243c85509
Jacob Tyo <jtyo@cs.cmu.edu>
2020-01-06 21:58:57 -0500

    small exp changes

--- regression/cavia_exp.sh
+++ regression/cavia_exp.sh
@@ -9,6 +9,10 @@ for num_fns in "${anum_fns[@]}"; do
   for num_inner_updates in "${anum_inner_updates[@]}"; do
     for tasks_per_metaupdate in "${atasks_per_metaupdate[@]}"; do
       tsk=${num_fns}__${num_inner_updates}_${tasks_per_metaupdate}
+      # maml
+      python ./main.py --maml --num_fns ${num_fns} --tasks_per_metaupdate ${tasks_per_metaupdate} \
+        --num_inner_updates ${num_inner_updates}  --id "${tsk}"
+      # cavia
       python ./main.py --num_fns ${num_fns} --tasks_per_metaupdate ${tasks_per_metaupdate} \
         --num_inner_updates ${num_inner_updates}  --id "${tsk}"
     done
