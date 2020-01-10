9095b66e2b500c42b8b061129d3331a882bb3f04
Jacob Tyo <jtyo@andrew.cmu.edu>
2020-01-10 09:56:35 -0500

    more experiments and analysis

--- regression/cavia_exp.sh
+++ regression/cavia_exp.sh
@@ -8,12 +8,12 @@ atasks_per_metaupdate=(25)
 for num_fns in "${anum_fns[@]}"; do
   for num_inner_updates in "${anum_inner_updates[@]}"; do
     for tasks_per_metaupdate in "${atasks_per_metaupdate[@]}"; do
-      tsk=${num_fns}__${num_inner_updates}_${tasks_per_metaupdate}
+      tsk='long'${num_fns}__${num_inner_updates}_${tasks_per_metaupdate}
       # maml
-      python ./main.py --maml --num_fns ${num_fns} --tasks_per_metaupdate ${tasks_per_metaupdate} \
+      python ./main.py --maml --n_iter 100000 --num_fns ${num_fns} --tasks_per_metaupdate ${tasks_per_metaupdate} \
         --num_inner_updates ${num_inner_updates}  --id "${tsk}"
       # cavia
-      python ./main.py --num_fns ${num_fns} --tasks_per_metaupdate ${tasks_per_metaupdate} \
+      python ./main.py --n_iter 100000 --num_fns ${num_fns} --tasks_per_metaupdate ${tasks_per_metaupdate} \
         --num_inner_updates ${num_inner_updates}  --id "${tsk}"
     done
   done
--- regression/cva_exps.sh
+++ regression/cva_exps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 anum_fns=($1)
-anum_eval_updates=(1 10 100)
+anum_eval_updates=(10 100)
 anum_inner_updates=(0)
 atasks_per_metaupdate=(25)
 
@@ -11,13 +11,13 @@ for num_fns in "${anum_fns[@]}"; do
     for num_inner_updates in "${anum_inner_updates[@]}"; do
       for tasks_per_metaupdate in "${atasks_per_metaupdate[@]}"; do
         # normal
-        tsk='100k_'${num_fns}_${num_eval_updates}_${num_inner_updates}_${tasks_per_metaupdate}
-        python ./main.py --cva --num_fns ${num_fns} --lr_inner 0.001 --lr_meta 0.001 \
+        tsk='long100k_'${num_fns}_${num_eval_updates}_${num_inner_updates}_${tasks_per_metaupdate}
+        python ./main.py --cva --n_iter 100000 --num_fns ${num_fns} --lr_inner 0.001 --lr_meta 0.001 \
           --tasks_per_metaupdate ${tasks_per_metaupdate} --num_inner_updates ${num_inner_updates} \
           --num_eval_updates ${num_eval_updates} --id "${tsk}"
         # reinitialize embeddings
-        tsk='remb100k_'${num_fns}_${num_eval_updates}_${num_inner_updates}_${tasks_per_metaupdate}
-        python ./main.py --cva --num_fns ${num_fns} --lr_inner 0.001 --lr_meta 0.001 \
+        tsk='longremb100k_'${num_fns}_${num_eval_updates}_${num_inner_updates}_${tasks_per_metaupdate}
+        python ./main.py --cva --n_iter 100000 --num_fns ${num_fns} --lr_inner 0.001 --lr_meta 0.001 \
           --tasks_per_metaupdate ${tasks_per_metaupdate} --num_inner_updates ${num_inner_updates} \
           --num_eval_updates ${num_eval_updates} --id "${tsk}" --reinit_emb
       done
