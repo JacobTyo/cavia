97deb62d4cd464a7dc969ea68589a79ded8165b5
Jake <jtyo@cs.cmu.edu>
2020-01-22 15:04:26 -0500

    experiment sync

--- regression/cva_exps_lowlr.sh
+++ regression/cva_exps_lowlr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 anum_fns=($1)
-anum_eval_updates=(20 50)
+anum_eval_updates=(40 100)
 anum_inner_updates=(0)
 atasks_per_metaupdate=(1)
 # reset_nums=(1)
@@ -12,8 +12,8 @@ for num_fns in "${anum_fns[@]}"; do
       for tasks_per_metaupdate in "${atasks_per_metaupdate[@]}"; do
 #	for reset_num in "${reset_nums[@]}"; do 
           # normal
-          tsk='longeval'_${reset_num}_${num_fns}_${num_eval_updates}_${num_inner_updates}_${tasks_per_metaupdate}
-          python ./main.py --cva --n_iter 400000 --num_fns ${num_fns} --lr_inner 0.001 --lr_meta 0.001 \
+          tsk='lowlr'_${reset_num}_${num_fns}_${num_eval_updates}_${num_inner_updates}_${tasks_per_metaupdate}
+          python ./main.py --cva --n_iter 400000 --num_fns ${num_fns} --lr_inner 0.0001 --lr_meta 0.0001 \
             --tasks_per_metaupdate ${tasks_per_metaupdate} --num_inner_updates ${num_inner_updates} \
             --num_eval_updates ${num_eval_updates} --id "${tsk}" 
 #        done
